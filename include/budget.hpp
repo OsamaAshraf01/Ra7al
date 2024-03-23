ed08b0f2ef1d8f1658fc849a66e63cb6c1c8acd5
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2024-03-23 22:58:43 +0200

    Budget header and source

--- include/budget.hpp
+++ include/budget.hpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#pragma once
 #include <string>
-using namespace std;
 
 class Budget
 {
@@ -14,97 +13,27 @@ private:
 
 public:
     // Default constructor
-    Budget()
-    {
-        totalBudget = 0;
-        flightBudget = 0;
-        hotelBudget = 0;
-        foodBudget = 0;
-        entertainmentBudget = 0;
-        shoppingBudget = 0;
-    }
+    Budget();
 
     // Constructor with parameters
-    Budget(double totalBudgetInput, double flightBudgetInput, double hotelBudgetInput, double foodBudgetInput, double entertainmentBudgetInput, double shoppingBudgetInput)
-    {
-        totalBudget = totalBudgetInput;
-        flightBudget = flightBudgetInput;
-        hotelBudget = hotelBudgetInput;
-        foodBudget = foodBudgetInput;
-        entertainmentBudget = entertainmentBudgetInput;
-        shoppingBudget = shoppingBudgetInput;
-    }
+    Budget(double totalBudgetInput, double flightBudgetInput, double hotelBudgetInput, double foodBudgetInput, double entertainmentBudgetInput, double shoppingBudgetInput);
 
     // Setters
-    void setTotalBudget(double newTotalBudget)
-    {
-        totalBudget = newTotalBudget;
-    }
-
-    void setFlightBudget(double newFlightBudget)
-    {
-        flightBudget = newFlightBudget;
-    }
-
-    void setHotelBudget(double newHotelBudget)
-    {
-        hotelBudget = newHotelBudget;
-    }
-
-    void setFoodBudget(double newFoodBudget)
-    {
-        foodBudget = newFoodBudget;
-    }
-
-    void setEntertainmentBudget(double newEntertainmentBudget)
-    {
-        entertainmentBudget = newEntertainmentBudget;
-    }
-
-    void setShoppingBudget(double newShoppingBudget)
-    {
-        shoppingBudget = newShoppingBudget;
-    }
+    void setTotalBudget(double newTotalBudget);
+    void setFlightBudget(double newFlightBudget);
+    void setHotelBudget(double newHotelBudget);
+    void setFoodBudget(double newFoodBudget);
+    void setEntertainmentBudget(double newEntertainmentBudget);
+    void setShoppingBudget(double newShoppingBudget);
 
     // Getters
-    double getTotalBudget() const
-    {
-        return totalBudget;
-    }
-
-    double getFlightBudget() const
-    {
-        return flightBudget;
-    }
-
-    double getHotelBudget() const
-    {
-        return hotelBudget;
-    }
-
-    double getFoodBudget() const
-    {
-        return foodBudget;
-    }
-
-    double getEntertainmentBudget() const
-    {
-        return entertainmentBudget;
-    }
-
-    double getShoppingBudget() const
-    {
-        return shoppingBudget;
-    }
+    double getTotalBudget() const;
+    double getFlightBudget() const;
+    double getHotelBudget() const;
+    double getFoodBudget() const;
+    double getEntertainmentBudget() const;
+    double getShoppingBudget() const;
 
     // Display budget
-    void displayBudget()
-    {
-        cout << "Total budget: " << totalBudget << endl;
-        cout << "Flight budget: " << flightBudget << endl;
-        cout << "Hotel budget: " << hotelBudget << endl;
-        cout << "Food budget: " << foodBudget << endl;
-        cout << "Entertainment budget: " << entertainmentBudget << endl;
-        cout << "Shopping budget: " << shoppingBudget << endl;
-    }
+    void displayBudget();
 };
