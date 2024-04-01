4cd3e64b98c2d26cd2452983d251302942f0a0f8
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-01 13:56:53 +0200

    Add Admin Class (.cpp/.h)

---
+++ include/Admin.h
@@ -0,0 +1,14 @@
+//
+// Created by OSAMA ASHRAF on 4/1/2024.
+//
+
+#ifndef RA7AL_ADMIN_H
+#define RA7AL_ADMIN_H
+
+
+class Admin {
+
+};
+
+
+#endif //RA7AL_ADMIN_H
---
+++ src/Admin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OSAMA ASHRAF on 4/1/2024.
+//
+
+#include "../include/Admin.h"
