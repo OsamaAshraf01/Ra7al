02da97c3fcd0c4888cab73b4ef36770eb9eaa7c7
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-01 14:15:06 +0200

    Admin Class: Add prototypes for methods

--- include/Admin.h
+++ include/Admin.h
@@ -5,9 +5,56 @@
 #ifndef RA7AL_ADMIN_H
 #define RA7AL_ADMIN_H
 
+#include "Person.h"
 
-class Admin {
+class Admin : public Person {
+private:
 
+public:
+    // Constructors
+
+
+    // Destructors
+
+
+    // Setters
+
+
+    // Getters
+
+
+    // Methods
+    void showUsers();
+    void showUser(int id);
+    void addUser();
+    void deleteUser(int id);
+
+    void showAirlines();
+    void showAirline(int id);
+    void addAirline();
+    void deleteAirline(int id);
+
+    void showFlights();
+    void showFlight(int id);
+    void addFlight();
+    void deleteFlight(int id);
+
+    void showHotels();
+    void showHotel(int id);
+    void addHotel();
+    void deleteHotel(int id);
+
+    void showPaymentMethods();
+    void showPaymentMethod(int id);
+    void addPaymentMethod();
+    void deletePaymentMethod(int id);
+
+
+    void editSettings();
+
+
+
+    // Overloaded Operators
 };
 
 
