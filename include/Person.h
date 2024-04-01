05dff0777a3295d37d428c112200483bad16b071
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-01 14:04:34 +0200

    Update Person Class (remove redundant data members)

--- include/Person.h
+++ include/Person.h
@@ -8,42 +8,26 @@ class Person
 private:
     static int currentID;
     int id;
-    int age;
-    string sex;
-    int budget;
     string name;
     string email;
-    string address;
-    string phoneNumber;
 
 public:
     // Default constructor
     Person();
 
     // Constructor with parameters
-    Person(int ageInput, string sexInput, int budgetInput, string nameInput, string emailInput, string addressInput, string phoneNumberInput);
+    Person(string nameInput, string emailInput);
 
     // Make sure the email is in the correct format
     bool isValidEmail(const string &email) const;
 
     // Setters
-    void setAge(int newAge);
-    void setSex(string newSex);
-    void setBudget(int newBudget);
     void setName(string newName);
     void setEmail(string newEmail);
-    void setAddress(string newAddress);
-    void setPhoneNumber(string newPhoneNumber);
 
     // Getters
-    int getID();
-    int getAge();
-    string getSex();
-    int getBudget();
     string getName();
     string getEmail();
-    string getAddress();
-    string getPhoneNumber();
 
     void printDetails();
 };
--- src/Person.cpp
+++ src/Person.cpp
@@ -9,26 +9,15 @@ int Person::currentID = 0;
 Person::Person()
 {
     id = currentID++;
-    age = 0;
-    sex = "N/A";
-    budget = 0;
     name = "Unknown";
     email = "N/A";
-    address = "N/A";
-    phoneNumber = "N/A";
 }
 
 // Constructor with parameters
-Person::Person(int ageInput, string sexInput, int budgetInput, string nameInput, string emailInput, string addressInput, string phoneNumberInput)
+Person::Person(string nameInput, string emailInput)
 {
-    id = currentID++;
-    age = ageInput;
-    sex = sexInput;
-    budget = budgetInput;
     name = nameInput;
     email = emailInput;
-    address = addressInput;
-    phoneNumber = phoneNumberInput;
 }
 
 // Make sure the email is in the correct format
@@ -39,84 +28,18 @@ bool Person::isValidEmail(const string &email) const
 }
 
 // Setters
-void Person::setAge(int newAge)
-{
-    age = newAge;
-}
-
-void Person::setSex(string newSex)
-{
-    sex = newSex;
-}
-
-void Person::setBudget(int newBudget)
-{
-    budget = newBudget;
-}
-
-void Person::setName(string newName)
-{
-    name = newName;
-}
-
-void Person::setEmail(string newEmail)
-{
-    email = newEmail;
-}
-
-void Person::setAddress(string newAddress)
-{
-    address = newAddress;
-}
-
-void Person::setPhoneNumber(string newPhoneNumber)
-{
-    phoneNumber = newPhoneNumber;
-}
+void Person::setName(string newName){name = newName;}
+void Person::setEmail(string newEmail){email = newEmail;}
 
 // Getters
-int Person::getID()
-{
-    return id;
-}
+string Person::getName(){return name;}
+string Person::getEmail(){return email;}
 
-int Person::getAge()
-{
-    return age;
-}
 
-string Person::getSex()
-{
-    return sex;
-}
-
-int Person::getBudget()
-{
-    return budget;
-}
-
-string Person::getName()
-{
-    return name;
-}
-
-string Person::getEmail()
-{
-    return email;
-}
-
-string Person::getAddress()
-{
-    return address;
-}
-
-string Person::getPhoneNumber()
-{
-    return phoneNumber;
-}
-
+// Methods
 void Person::printDetails()
 {
-    cout << "ID: " << id << "\nAge: " << age << "\nSex: " << sex << "\nBudget: " << budget << "\nName: " << name << "\nEmail: " << email;
-    cout << "\nAddress: " << address << "\nPhone Number: " << phoneNumber << endl;
+    cout << "ID: " << id
+         << "\nName: " << name
+         << "\nEmail: " << email <<'\n';
 }
