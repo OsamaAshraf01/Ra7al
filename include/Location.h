a4533c4a3270e3aebee0fee7830b45e8e5277603
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-05 05:00:13 +0200

    Add Location Class

---
+++ include/Location.h
@@ -0,0 +1,14 @@
+//
+// Created by OSAMA ASHRAF on 4/5/2024.
+//
+
+#ifndef RA7AL_LOCATION_H
+#define RA7AL_LOCATION_H
+
+
+class Location {
+
+};
+
+
+#endif //RA7AL_LOCATION_H
---
+++ src/Location.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OSAMA ASHRAF on 4/5/2024.
+//
+
+#include "../include/Location.h"
