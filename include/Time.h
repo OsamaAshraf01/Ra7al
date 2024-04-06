9b847efa7a518aed66c42104775f6bb67c253084
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-06 06:12:15 +0200

    Add Time class(.h/.cpp)

---
+++ include/Time.h
@@ -0,0 +1,14 @@
+//
+// Created by OSAMA ASHRAF on 4/6/2024.
+//
+
+#ifndef RA7AL_TIME_H
+#define RA7AL_TIME_H
+
+
+class Time {
+
+};
+
+
+#endif //RA7AL_TIME_H
---
+++ src/Time.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OSAMA ASHRAF on 4/6/2024.
+//
+
+#include "../include/Time.h"
