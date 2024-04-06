9fbc97e3c2aef6614576b9f2f8e23044a6ccfc2d
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-06 22:55:39 +0200

    Start Implementation of Database Class

--- src/Database.cpp
+++ src/Database.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "../include/Database.h"
+#include "../include/publicFunctions.h"
+
+// Constructors
+Database::Database(string databasePath): path(databasePath), fout(path, ios::app), fin(path) {}
+
+// Methods
+void Database::insert(vector<string> values) {
+    fout << '\n' << join(values, ",");
+}
+
+void Database::close(){this->fout.close();}
