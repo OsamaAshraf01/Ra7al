9cd4b8304ba483eb268f5e5512f944d37b52203e
Ahmed Hamdy Kotp <160382377+AhmedHamdyKotp@users.noreply.github.com>
2024-04-06 18:00:24 +0200

    Update Budget.cpp
    the modified piece

--- src/Budget.cpp
+++ src/Budget.cpp
@@ -2,51 +2,71 @@
 
 
 
-#include "Budget.h"
+#include "../include/Budget.h"
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 // Constructor 
 Budget::Budget() : total_budget(0.0) {}
+Budget::Budget(const double& total): 
+total_budget(total){}
 
 // Add a new budget category or update an existing one
-void Budget::add_category(const string& category_name) {
-    categories[category_name] = 0.0;
+void Budget::add_category(const string& category_name){
+    for (auto& x : categories) {
+        if (category_name != x.first) {
+            categories[category_name] = 0.0;
+        }
+        else {
+            cout<<"THis category is already exist !\n"
+        }
+    }
 }
 
 // Get the balance of a specific category
 double Budget::getBalance(const string& category_name) const {
     auto bal = categories.find(category_name);
-    return bal != categories.end() ? bal->second : 0.0;
+    return bal != categories.end() ? bal->second : -1;
 }
 
 // Spend from a category
 void Budget::spend(const string& category_name, double amount) {
-    if (categories[category_name] >= amount) {
-        categories[category_name] -= amount; total_budget -= amount;
+    for (auto& x : categories) {
+        if (category_name == x.first) {
+        
+            if (categories[category_name] >= amount) {
+                categories[category_name] -= amount; total_budget = getRemaining();
+            }
+        }
     }
 }
 
 // Deposit to a category
 void Budget::deposit(const string& category_name, double amount) {
-    categories[category_name] += amount;
-    total_budget += amount;
+    for (auto& x : categories) {
+        if (category_name == x.first) {
+
+            categories[category_name] += amount; total_budget += amount;
+        }
+    }
 }
 
 // Calculate the total amount spent
 double Budget::getTotalSpent() const {
-    double total = 0.0;
-    for (const auto& item : categories) {
-        total += item.second;
-    }
-    return total;
+    return total_budget - getRemaining();
 }
+
 
 // Get the overall remaining budget
 double Budget::getRemaining() const {
-    return total_budget - getTotalSpent();
+    double totalRemaining = 0.0;
+    for (const auto& item : categories) {
+        totalRemaining += item.second; 
+    }
+    return totalRemaining;
 }
+
 
 // Provide a formatted overview of the budget
 void Budget::overview() const {
