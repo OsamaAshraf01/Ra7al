9f6de3cecf7590d3d085c6c827cb25d2afb4e75c
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-06 07:01:05 +0200

    Add main functionalities od Time class.

--- include/Time.h
+++ include/Time.h
@@ -4,10 +4,34 @@
 
 #ifndef RA7AL_TIME_H
 #define RA7AL_TIME_H
-
+#include <iostream>
+using namespace std;
 
 class Time {
+private:
+    int seconds, minutes, hours;
+    string period;
+public:
+    // Constructors
+    Time();
+    Time(int seconds, int minutes, int hours, string period);
 
+    // Getters
+    int getSeconds() const;
+    int getMinutes() const;
+    int getHours() const;
+
+    // Setters
+    bool setSeconds(int s);
+    bool setMinutes(int m);
+    bool setHours(int h);
+
+    // Methods
+    bool isValid();
+    void addSeconds(int s);
+    void addMinutes(int m);
+    void addHours(int h);
+    string toString();
 };
 
 
