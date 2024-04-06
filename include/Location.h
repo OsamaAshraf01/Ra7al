c8040f6b2e541aabef6025fd658555b794b51071
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-06 13:12:43 +0200

    Add examples in Location.h

--- include/Location.h
+++ include/Location.h
@@ -1,6 +1,20 @@
 //
 // Created by OSAMA ASHRAF on 4/5/2024.
 //
+/* ---Validation---
+    Location l;   double lat, lon;
+    do{
+        cout<<"Enter Latitude: ";
+        cin>>lat;
+    } while(!l.setLatitude(lat) && (cout << "Invalid Value! "));
+    do{
+        cout<<"Enter Longitude: ";
+        cin>>lon;
+    } while(!l.setLongitude(lon) && (cout << "Invalid Value! "));
+
+    cout<<l.toString();
+*/
+
 
 #ifndef RA7AL_LOCATION_H
 #define RA7AL_LOCATION_H
