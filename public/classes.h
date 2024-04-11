118ce5a9f3bc7707c793035a00b59d6190894caa
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-11 23:16:12 +0200

    Update: classes.h and functions.h in public directory

--- public/classes.h
+++ public/classes.h
@@ -5,6 +5,7 @@
 #ifndef RA7AL_CLASSES_H
 #define RA7AL_CLASSES_H
 #include <bits/stdc++.h>
+using namespace std;
 
 /*=====List Example========
     List l1, l2, l3;
@@ -20,34 +21,33 @@
     cout<<l3;
  */
 //=====List definition=====
-class List{
+class List {
 private:
-    vector< variant<int, double, string, char> > values;
+    vector<variant<int, double, string, char> > values;
 public:
     /// Constructor
     template<typename... Args>
-    List(Args... args){
+    List(Args... args) {
         initialize(args...);
     }
 
     template<typename T, typename... Args>
-    void initialize(T first, Args... rest){
+    void initialize(T first, Args... rest) {
         values.emplace_back(first);
         initialize(rest...);
     }
 
     // Base Case
-    void initialize(){}
-
+    void initialize() {}
 
 
     /// Methods
-    void append(const variant<int, double, string, char>& e){
+    void append(const variant<int, double, string, char> &e) {
         values.emplace_back(e);
     }
 
 
-    void printItem(int index){
+    void printItem(int index) {
         auto item = values[index];
         if (holds_alternative<int>(item)) {
             cout << get<int>(item);
@@ -63,25 +63,24 @@ public:
     }
 
 
-    int size(){return (int)values.size();}
+    int size() { return (int) values.size(); }
 
 
 
     /// Iterators
     using iterator = vector<variant<int, double, string, char>>::iterator;
+
     iterator begin() { return values.begin(); }
+
     iterator end() { return values.end(); }
 
 
-
-
-
     /// Operators
-    friend ostream& operator<<(ostream& os, List l){
+    friend ostream &operator<<(ostream &os, List l) {
         os << '{';
-        for(int i=0 ; i<l.size() ; i++) {
+        for (int i = 0; i < l.size(); i++) {
             l.printItem(i);
-            os << (i <l.size()-1 ? ", " : "");
+            os << (i < l.size() - 1 ? ", " : "");
         }
 
         os << "}";
@@ -89,14 +88,16 @@ public:
         return os;
     }
 
-    List operator +(List& other){
+    List operator+(List &other) {
         List temp = *this;
-        for(const auto& e : other){
+        for (const auto &e: other) {
             temp.values.emplace_back(e);
         }
 
         return temp;
     }
+
+    auto operator[](int index) { return values[index]; }
 
 };
 
--- public/functions.h
+++ public/functions.h
@@ -5,10 +5,11 @@
 #ifndef RA7AL_FUNCTIONS_H
 #define RA7AL_FUNCTIONS_H
 #include <bits/stdc++.h>
+#include "classes.h"
 using namespace std;
 
 // A function to split a string based on specific delimiter (space by default)
-static vector<string> split(string &str, char delimiter = ' ');
+static List split(string &str, char delimiter = ' ');
 
 
 // A function to join vector of string values by a delimiter
@@ -16,47 +17,34 @@ static string join(vector<string> &values, const string &delimiter);
 
 
 // A function to print a vector
-template <typename T>
+template<typename T>
 static void printVec(vector<T> v);
 
 
+// A function to convert string to appropriate data type
+static variant<int, double, string, char> Transform(const string &str);
 
 
-
-
-
-
-
-
-
-
-
-///==========================================================
-///========================Definition========================
-///==========================================================
-vector<string> split(string &str, char delimiter)
-{
-    vector<string> result;
+///====================================================================================
+///=====================================Definition=====================================
+///====================================================================================
+List split(string &str, char delimiter) {
+    List result;
     string temp;
-    for (char i : str)
-    {
-        if (i == delimiter)
-        {
-            result.push_back(temp);
+    for (char i: str) {
+        if (i == delimiter) {
+            result.append(Transform(temp));
             temp = "";
-        }
-        else
-        {
+        } else {
             temp += i;
         }
     }
-    result.push_back(temp);
+    result.append(Transform(temp));
     return result;
 }
 
 
-string join(vector<string> &values, const string &delimiter)
-{
+string join(vector<string> &values, const string &delimiter) {
     ostringstream oss;
     for (int i = 0; i < values.size(); i++)
         oss << values[i] << (i + 1 != values.size() ? delimiter : "");
@@ -65,12 +53,62 @@ string join(vector<string> &values, const string &delimiter)
 }
 
 
-template <typename T>
-void printVec(vector<T> v){
-    cout<<'{';
-    for(int i=0 ; i<v.size() ; i++)
-        cout<<v[i]<<(i < v.size()-1 ? ", " : "");
-    cout<<"}\n";
+template<typename T>
+void printVec(vector<T> v) {
+    cout << '{';
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << (i < v.size() - 1 ? ", " : "");
+    cout << "}\n";
 }
 
+
+enum DataType {
+    Integer, Double, Char, String
+};
+
+static DataType determineType(const string &str) {
+    istringstream iss(str);
+    char c;
+    iss >> c;
+    if (!isdigit(c) && c != '-') {  // string or char
+        if (str.size() > 1)
+            return String;
+        else
+            return Char;
+    } else { // int or double
+        if (str.find('.') == -1)
+            return Integer;
+        else
+            return Double;
+    }
+
+}
+
+template<typename T>
+static T convert(const std::string &str) {
+    std::istringstream iss(str);
+    T value;
+    iss >> value;
+    return value;
+}
+
+static variant<int, double, string, char> Transform(const string &str) {
+    DataType type = determineType(str);
+
+    // Convert the string to its appropriate data type
+    switch (type) {
+        case Integer:
+            return convert<int>(str);
+        case Double:
+            return convert<double>(str);
+        case String:
+            return str;
+        case Char:
+            return str[0];
+        default: {
+            cerr << "Unexpected data type" << endl;
+            return {};
+        }
+    }
+}
 #endif // RA7AL_FUNCTIONS_H
