5fc126dcbdcedd5f2a1bee39d89e8c1fd78ee302
Khaled waleed <s-khaled.talaat@zewailcity.edu.eg>
2024-04-11 22:05:24 +0200

    create Hotal class

--- include/Airline.h
+++ include/Airline.h
@@ -1,4 +1,4 @@
-/*#ifndef AIRLINE_H
+#ifndef AIRLINE_H
 #define AIRLINE_H
 
 #include <iostream>
@@ -37,54 +37,10 @@ public:
     void printDetails();
 };
 
-#endif // AIRLINE_H*/
+#endif // AIRLINE_H
 
 
 
 
-/*#ifndef HOTEL_H
-#define HOTEL_H
 
-#include <iostream> 
-#include <string>
-#include <vector>
-
-using namespace std;
-
-class Hotel {
-
-private:
-    string name;
-    string location;
-    string type;
-    string description;
-    vector<string> amenities;
-    double pricing;
-
-public:
-
-    // Setters
-    void setName(string name);
-    void setLocation(string location);
-    void setType(string type);
-    void setDescription(string description);
-    void setAmenities(vector<string> amenities);
-    void setPricing(double pricing);
-
-    // Getters
-    string getName();
-    string getLocation();
-    string getType();
-    string getDescription();
-    vector<string> getAmenities();
-    double getPricing();
-
-    // Methods
-    void showDetails();
-    void showPricing();
-    void showAmenities();
-    void addAmenity(string amenity);
-};
-
-#endif*/
 
---
+++ src/Hotel.cpp
@@ -0,0 +1,111 @@
+#include"../include/Hotel.h"
+#include <iostream> 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Hotel {
+
+private:
+    string name;
+    string location;
+    string type;
+    string description;
+    vector<string> amenities;
+    double pricing;
+
+public:
+
+
+    // Setters
+    void setName(string name)
+    {
+        this->name = name;
+    }
+
+    void setLocation(string location)
+    {
+        this->location = location;
+    }
+
+    void setType(string type)
+    {
+        this->type = type;
+    }
+
+    void setDescription(string description)
+    {
+        this->description = description;
+    }
+
+    void setAmenities(vector<string> amenities)
+    {
+        this->amenities = amenities;
+    }
+
+    void setPricing(double pricing)
+    {
+        this->pricing = pricing;
+    }
+
+
+    // Getters
+    string getName()
+    {
+        return name;
+    }
+
+    string getLocation()
+    {
+        return location;
+    }
+
+    string getType()
+    {
+        return type;
+    }
+
+    string getDescription()
+    {
+        return description;
+    }
+
+    vector<string> getAmenities()
+    {
+        return amenities;
+    }
+
+    double getPricing()
+    {
+        return pricing;
+    }
+
+
+    // Methods
+    void showDetails()
+    {
+        cout << "Name: " << name << "\n";
+        cout << "Location: " << location << "\n";
+        cout << "Type: " << type << "\n";
+        cout << "Description: " << description << "\n";
+        cout << "Amenities:\n";
+
+    }
+
+    void showPricing()
+    {
+        cout << "Pricing: " << pricing << "\n";
+    }
+
+    void showAmenities()
+    {
+        cout << "Amenities: " << "amenities " << "\n";
+
+    }
+
+    void addAmenity(string amenity)
+    {
+        amenities.push_back(amenity);
+    }
+};
