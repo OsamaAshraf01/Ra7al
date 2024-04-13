349dcf55e39dcd9e1eb62b049fe8d6091946a7a0
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-14 00:14:18 +0200

    Creat: Series Class

---
+++ include/Series.h
@@ -0,0 +1,14 @@
+//
+// Created by OSAMA ASHRAF on 4/14/2024.
+//
+
+#ifndef RA7AL_SERIES_H
+#define RA7AL_SERIES_H
+
+
+class Series {
+
+};
+
+
+#endif //RA7AL_SERIES_H
---
+++ src/Series.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OSAMA ASHRAF on 4/14/2024.
+//
+
+#include "../include/Series.h"
