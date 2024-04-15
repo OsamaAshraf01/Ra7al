f50f086d53abf3bb7bdccbd78813df3b211105f1
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-15 11:49:17 +0200

    Create: Duration Class

---
+++ include/Duration.h
@@ -0,0 +1,14 @@
+//
+// Created by OSAMA ASHRAF on 4/15/2024.
+//
+
+#ifndef RA7AL_DURATION_H
+#define RA7AL_DURATION_H
+
+
+class Duration {
+
+};
+
+
+#endif //RA7AL_DURATION_H
---
+++ src/Duration.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OSAMA ASHRAF on 4/15/2024.
+//
+
+#include "../include/Duration.h"
