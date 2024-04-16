22ff31c3fee777138a6a921d8b382d3a818f0ff7
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-16 12:47:04 +0200

    Create: Airport Class (.h/.cpp)

---
+++ include/Airport.h
@@ -0,0 +1,68 @@
+//
+// Created by OSAMA ASHRAF on 4/15/2024.
+//
+
+#ifndef RA7AL_AIRPORT_H
+#define RA7AL_AIRPORT_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Country.h"
+#include "Airline.h"
+#include "Flight.h"
+#include "Location.h"
+
+using namespace std;
+
+class Airport
+{
+private:
+    string name;
+    string code;
+    Country country;
+    Location Loc;
+    vector <Airline> airlines;
+    vector <Flight> flights;
+
+public:
+
+    //setters
+    void setName(string name);
+
+    void setCode(string Code);
+
+    void setCountry(Country _country);
+
+    void setLoc(Location loc);
+
+    void setAirlines(vector<Airline> Airlines);
+
+    void setFlights(vector <Flight> Flights);
+
+
+    //getters
+    string getName();
+
+    string getCode();
+
+    Country getCountry();
+
+    Location getLoc();
+
+    vector <Airline> getAirlines();
+
+    vector <Flight> getFlights();
+
+    //methods 
+    vector<Flight> getFlightsTo(const string& flightCode);
+
+    vector<Airline> getFlightsByAirline(const string& airlineName);
+
+    vector<string> getSupportedAirlines();
+
+    double distanceTo(const Airport& other);
+
+};
+
+
+#endif //RA7AL_AIRPORT_H
