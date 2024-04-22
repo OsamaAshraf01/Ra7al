56f46b260520be110e304844e2ef62a271bfd898
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-22 21:38:01 +0200

    Modify: User class

--- headers/User.h
+++ headers/User.h
@@ -5,10 +5,14 @@
 #ifndef RA7AL_USER_H
 #define RA7AL_USER_H
 #include "Person.h"
+#include "DataFrame/DataFrame.h"
+#include "config.h"
 
 class User : public Person{
+public:
+    using Person::Person;
 
+    void Register();
 };
-
 
 #endif //RA7AL_USER_H
--- src/User.cpp
+++ src/User.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "headers/User.h"
+
+void User::Register(){
+    DataFrame registered(CONFIG.registered_path);
+
+    Case x;
+    x.header = registered.Header();
+    x["Name"] = name;
+    x["Email"] = email;
+    x["Password"] = password;
+    x["Type"] = "user";
+
+    registered.INSERT(x);
+
+    registered.save();
+}
