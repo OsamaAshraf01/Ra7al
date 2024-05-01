11ebb184aff3d6d832c393ae59a804d73f0c5282
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-05-01 07:12:38 +0300

    Update: User class

--- headers/User.h
+++ headers/User.h
@@ -1,43 +1,18 @@
+//
+// Created by [] on []
+//
+
 #ifndef RA7AL_USER_H
 #define RA7AL_USER_H
 #include "Person.h"
-#include <vector>
-#include <string>
-#include "Itinerary.h"
-#include "PaymentMethod.h"
-#include "Budget.h"
-#include "Language.h"
-#include "Person.h"
-using namespace std;
+#include "DataFrame/DataFrame.h"
+#include "config.h"
 
-class User : public Person // User inherits from Person
-{
-private:
-    vector<Itinerary> itineraries;
-    vector<PaymentMethod> payments;
-    Budget budget;
-    vector<Language> languages;
-    vector<string> preferences;
-
+class User : public Person{
 public:
-    User();
-    User(string nameInput, string emailInput);
+    using Person::Person;
 
-    // Setters
-    void addItinerary(Itinerary newItinerary);
-    void addPayment(PaymentMethod newPayment);
-    void setBudget(Budget newBudget);
-    void addLanguage(Language newLanguage);
-
-    // Getters
-    vector<Itinerary> getItineraries();
-    vector<PaymentMethod> getPayment();
-    Budget getBudget();
-    vector<Language> getLanguages();
-
-    // Methods
-    void showPreferences();
-    void printDetails();
+    void Register();
 };
 
-#endif
+#endif //RA7AL_USER_H
--- src/User.cpp
+++ src/User.cpp
@@ -1,108 +1,20 @@
-#include "../headers/User.h"
-#include <iostream>
-#include <string>
-#include <regex>
-using namespace std;
+//
+// Created by [] on []
+//
 
-User::User()
-{
-    email = "";
-    password = "";
-    itineraries = {};
-    payments = {};
-    budget = Budget();
-    preferences = {};
-    language = Language();
-}
+#include "headers/User.h"
 
-User::User(string email, string password, vector<Itinerary> itineraries, vector<PaymentMethod> payments, Budget budget, vector<string> preferences, Language language)
-{
-    if (validEmail(email))
-    {
-        cout << "Email is valid" << endl;
-    }
-    else
-    {
-        cout << "Email is invalid" << endl;
-        return;
-    }
+void User::Register(){
+    DataFrame registered(CONFIG.registered_path);
 
-    this->email = email;
-    this->password = password;
-    this->itineraries = itineraries;
-    this->payments = payments;
-    this->budget = budget;
-    this->language = language;
-    this->preferences = preferences;
-}
+    Case x;
+    x.header = registered.Header();
+    x["Name"] = name;
+    x["Email"] = email;
+    x["Password"] = password;
+    x["Type"] = "user";
 
-void User::addItinerary(Itinerary newItinerary)
-{
-    itineraries.push_back(newItinerary);
-}
+    registered.INSERT(x);
 
-void User::addPaymentMethod(PaymentMethod newPayment)
-{
-    payments.push_back(newPayment);
-}
-
-void User::setBudget(Budget newBudget)
-{
-    budget = newBudget;
-}
-
-void User::addLanguage(Language newLanguage)
-{
-    language = newLanguage;
-}
-
-vector<Itinerary> User::getItineraries()
-{
-    return itineraries;
-}
-
-vector<PaymentMethod> User::getPaymentMethods()
-{
-    return payments;
-}
-
-Budget User::getBudget()
-{
-    return budget;
-}
-
-Language User::getLanguage()
-{
-    return language;
-}
-
-void User::showPreferences()
-{
-    for (int i = 0; i < preferences.size(); i++)
-    {
-        cout << preferences[i] << endl;
-    }
-}
-
-void User::printDetails()
-{
-    cout << "Email: " << email << endl;
-    cout << "Password: " << password << endl;
-    cout << "Itineraries: " << endl;
-    for (int i = 0; i < itineraries.size(); i++)
-    {
-        cout << itineraries[i].show() << endl;
-    }
-    cout << "Payments: " << endl;
-    for (int i = 0; i < payments.size(); i++)
-    {
-        cout << payments[i].getCardNumber() << endl; // to be implemented
-    }
-    cout << "Budget: " << budget.overview() << endl;
-    cout << "Language: " << language.getLanguage() << endl;
-    cout << "Preferences: " << endl;
-    for (int i = 0; i < preferences.size(); i++)
-    {
-        cout << preferences[i] << endl;
-    }
+    registered.save();
 }
